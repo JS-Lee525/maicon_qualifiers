d0518fbf736ded4e491cf8ca4b45bf251d384d7f
Jae Won Choi <djc0105@gmail.com>
2022-10-31 21:07:59 +0900

    patch dataset

--- gencd/options/train_options.py
+++ gencd/options/train_options.py
@@ -46,6 +46,7 @@ class TrainOptions():
         parser.add_argument('--dataset_mode', type=str, help='name of dataset class')
         parser.add_argument('--num_class', type=int, default=2, help='number of classes including background')
         parser.add_argument('--patch_size', default=256, type=int, help='input patch size')
+        parser.add_argument('--patch_overlap', default=0, type=float, help='for patch-based dataloader. not used in random crop')
         parser.add_argument('--batch_size', default=1, type=int, help='batch size for train')
         parser.add_argument('--batch_size_inference', default=1, type=int, help='batch size for inference')
         
