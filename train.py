4acd94c08080efb5926d32eb50d9f91bf1741a15
Jae Won Choi <djc0105@gmail.com>
2022-11-08 13:35:23 +0900

    add fold training

--- train.py
+++ train.py
@@ -9,20 +9,49 @@ from gencd.models import create_model
 from gencd.options import TrainOptions
 
 if __name__ == '__main__':
-    opt = TrainOptions().parse()
+    topt = TrainOptions().parse()
 
-    dm = MyDataModule(opt)
-    model = create_model(opt)
-    trainer = MyTrainer(dm)
-    
-    if opt.resume_from_checkpoint:
-        trainer.fit(model, dm, ckpt_path=opt.resume_from_checkpoint)
-    else:
-        trainer.fit(model, dm)
-    
-    if not opt.train_only:
-        # validation
-        #trainer.predict(model, dataloaders=dm.val_dataloader(), ckpt_path='best')
-        
-        # test
-        trainer.test(model, dm, ckpt_path='best')
+    if topt.fold and (not -1 in topt.fold):
+        for f in topt.fold:
+            opt = copy.deepcopy(topt)
+            opt.fold = f            
+            savedir = os.path.join(opt.save_dir, f'fold{f}')
+            os.makedirs(savedir, exist_ok=True)
+            opt.save_dir = savedir        
+            opt.wandb_name = os.sep.join(savedir.split(os.sep)[-3:])
+            
+            dm = MyDataModule(opt)
+            model = create_model(opt)
+            trainer = MyTrainer(dm)
+
+            if opt.resume_from_checkpoint:
+                trainer.fit(model, dm, ckpt_path=opt.resume_from_checkpoint)
+            else:
+                trainer.fit(model, dm)
+
+            if not opt.train_only:
+                # validation
+                #trainer.predict(model, dataloaders=dm.val_dataloader(), ckpt_path='best')
+
+                # test
+                trainer.test(model, dm, ckpt_path='best')
+                
+            wandb.finish()            
+            
+    else:    
+        opt = copy.deepcopy(topt)
+        dm = MyDataModule(opt)
+        model = create_model(opt)
+        trainer = MyTrainer(dm)
+
+        if opt.resume_from_checkpoint:
+            trainer.fit(model, dm, ckpt_path=opt.resume_from_checkpoint)
+        else:
+            trainer.fit(model, dm)
+
+        if not opt.train_only:
+            # validation
+            #trainer.predict(model, dataloaders=dm.val_dataloader(), ckpt_path='best')
+
+            # test
+            trainer.test(model, dm, ckpt_path='best')
