cd326168a5cb1e947280f67ee368fc69477b040f
KSY <sungyun1997@gmail.com>
2022-11-08 21:06:25 +0900

    train scripts

--- scripts/templates/train.sh
+++ scripts/templates/train.sh
@@ -5,11 +5,12 @@ exp_name=exp
 exp_number=1
 
 # data
-num_class=2
-datadir=/home/jwchoi/Downloads/LEVIR-CD-256
-dataset_mode=changeformer
+num_class=4
+datadir=/temp/data/01_data
+dataset_mode=MaiconPatchv0
 patch_size=256
 patch_overlap=0
+split=/temp/maicon-split.pkl
 
 # model
 model=cd_base
@@ -23,6 +24,7 @@ max_epochs=100
 lr=0.001
 lr_policy=linear
 optimizer=adamw
+fold=0
 
 # trainer
 callbacks=lr_ckpt_metricvalid
@@ -35,9 +37,18 @@ loggers=tb
 wandb_project=maicon
 
 # network
-net_module=gencd.models.networks.BIT
-net_config=config/BIT/bit-vitaev2.yaml
-load_pretrained_network=temp/pretrained/bit-rsp-vitaev2-s-cdd-weight.pt
+net_module=gencd.models.networks
+net_config=config/snunet_ch4.yaml
+load_pretrained_network=temp/pretrained/snunet-32-weight.pt
+
 
 # RUN
-python train.py --gpu_ids 0 --exp_name ${exp_name} --exp_number ${exp_number} --num_class ${num_class} --datadir ${datadir} --dataset_mode ${dataset_mode} --patch_size ${patch_size} --patch_overlap ${patch_overlap} --model ${model} --loss ${loss} --metric ${metric} --batch_size ${batch_size} --batch_size_inference ${batch_size_inference} --max_epochs ${max_epochs} --lr ${lr} --lr_policy ${lr_policy} --optimizer ${optimizer} --callbacks ${callbacks} --check_val_every_n_epoch ${check_val_every_n_epoch} --checkpoint_every_n_epochs ${checkpoint_every_n_epochs} --checkpoint_filename ${checkpoint_filename} --checkpoint_monitor ${checkpoint_monitor} --checkpoint_monitor_mode ${checkpoint_monitor_mode} --loggers ${loggers} --wandb_project ${wandb_project} --net_module ${net_module} --net_config ${net_config} --load_pretrained_network ${load_pretrained_network}
+python train.py \
+--gpu_ids 0 \
+--exp_name ${exp_name} --exp_number ${exp_number} \
+--seed_determinism 2147483647 \
+--num_class ${num_class} --datadir ${datadir} --dataset_mode ${dataset_mode} --patch_size ${patch_size} --patch_overlap ${patch_overlap} --dataset_split ${split} \
+--model ${model} --loss ${loss} --metric ${metric} \
+--batch_size ${batch_size} --batch_size_inference ${batch_size_inference} --max_epochs ${max_epochs} --lr ${lr} --lr_policy ${lr_policy} --optimizer ${optimizer} --fold ${fold} \
+--callbacks ${callbacks} --check_val_every_n_epoch ${check_val_every_n_epoch} --checkpoint_every_n_epochs ${checkpoint_every_n_epochs} --checkpoint_filename ${checkpoint_filename} --checkpoint_monitor ${checkpoint_monitor} --checkpoint_monitor_mode ${checkpoint_monitor_mode} --loggers ${loggers} --wandb_project ${wandb_project} --checkpoint_nooverwrite \
+--net_module ${net_module} --net_config ${net_config} --load_pretrained_network ${load_pretrained_network}
