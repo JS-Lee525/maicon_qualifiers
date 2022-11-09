d7f5e289f6a2da1314ce4fcfb23d687fef1825c4
Jae Won Choi <djc0105@gmail.com>
2022-11-09 12:14:42 +0900

    update test.sh for maicon

--- scripts/templates/test.sh
+++ scripts/templates/test.sh
@@ -6,20 +6,18 @@ phase=test
 load_pretrained_model="./runs/exp/00001/checkpoint/epoch=00049_val_loss=0.2245.ckpt"
 
 # data
-num_class=2
-datadir=/home/jwchoi/Downloads/LEVIR-CD-256
-dataset_mode=changeformer
+num_class=3
+datadir=/workspace/maicon_qualifiers/temp/data/01_data
+dataset_mode=maicon_patch_v0
 patch_size=256
 batch_size=16
-batch_size_inference=2
+batch_size_inference=1
 
 # model
-metric=f1_iou
+## metric=f1_iou
 
 # trainer
 callbacks=result
-loggers=csv
-wandb_project=maicon
 
 # RUN
-python test.py --gpu_ids 0 --run_base_dir ${run_base_dir} --phase ${phase} --load_pretrained_model ${load_pretrained_model} --num_class ${num_class} --datadir ${datadir} --dataset_mode ${dataset_mode} --patch_size ${patch_size} --batch_size ${batch_size} --batch_size_inference ${batch_size_inference} --metric ${metric} --callbacks ${callbacks} --loggers ${loggers} --wandb_project ${wandb_project}
+python test.py --gpu_ids 0 --run_base_dir ${run_base_dir} --phase ${phase} --load_pretrained_model ${load_pretrained_model} --num_class ${num_class} --datadir ${datadir} --dataset_mode ${dataset_mode} --patch_size ${patch_size} --batch_size ${batch_size} --batch_size_inference ${batch_size_inference} --callbacks ${callbacks} # --metric ${metric} --loggers ${loggers} --wandb_project ${wandb_project}
